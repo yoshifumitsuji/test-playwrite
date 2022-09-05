e1b1b02a8a7058e171369a6cdf73ac144b22c3fd
yoshifumitsuji <yoshifumi.tsuji.biz@gmail.com>
2022-09-05 18:12:36 +0900

    feat: first commit

---
+++ src/pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <Tutorial />
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage',
+})
+</script>
+
+<style lang="scss" scoped>
+@use '../styles/globals.scss';
+</style>
