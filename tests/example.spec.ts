54cae2c80809c68e9628e0c59a5c2b06b8643bc7
yoshifumitsuji <yoshifumi.tsuji.biz@gmail.com>
2022-09-07 13:34:16 +0900

    test for localhost

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -5,22 +5,22 @@ test('homepage has Playwright in title and get started link linking to the intro
 }) => {
   // await page.goto('https://google.com')
   // await page.goto('https://example.com')
-  await page.goto('https://playwright.dev/')
+  await page.goto('http://localhost:3000')
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/)
+  // // Expect a title "to contain" a substring.
+  // await expect(page).toHaveTitle(/Playwright/)
 
-  // create a locator
-  const getStarted = page.locator('text=Get Started')
+  // // create a locator
+  // const getStarted = page.locator('text=Get Started')
 
-  // Expect an attribute "to be strictly equal" to the value.
-  await expect(getStarted).toHaveAttribute('href', '/docs/intro')
+  // // Expect an attribute "to be strictly equal" to the value.
+  // await expect(getStarted).toHaveAttribute('href', '/docs/intro')
 
-  // Click the get started link.
-  await getStarted.click()
+  // // Click the get started link.
+  // await getStarted.click()
 
-  // Expects the URL to contain intro.
-  await expect(page).toHaveURL(/.*intro/)
+  // // Expects the URL to contain intro.
+  // await expect(page).toHaveURL(/.*intro/)
 
   // Execute VRT
   await expect(page).toHaveScreenshot('landing.png')
